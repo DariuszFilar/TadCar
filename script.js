81adbe253adbb130b9478e3302452a0831b6325c
darek.filar <darek.filar@gmail.com>
2022-05-05 12:38:27 +0200

    Added "bootstrap 5", "navbar" and "header"

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-<script></script>
+window.onload = function () {
+  var counter = 1; //instantiate a counter
+  var maxImage = 3; //the total number of images that are available
+  setInterval(function () {
+    document.querySelector("header").style.backgroundImage =
+      "url('images/" + (counter + 1) + ".jpg')";
+    if (counter + 1 == maxImage) {
+      counter = 0; //reset to start
+    } else {
+      ++counter; //iterate to next image
+    }
+    document.querySelector("header").style.transition = "2s";
+  }, 5000);
+};
